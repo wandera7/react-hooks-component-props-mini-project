384cf2c2c768356ddf9d00eb7257fdb3207470be
wandera7 <julius.wandera@student.moringaschool.com>
2022-07-13 09:01:36 +0300

    Done.

---
+++ src/components/ArticleList.js
@@ -0,0 +1,19 @@
+import Article from "./Article";
+
+function ArticleList({posts}){
+    return (
+        <main>
+        {
+            posts.map((post)=>{
+                return <Article key={post.id}
+                title={post.title}
+                date={post.date}
+                preview={post.preview} 
+                 />
+            })
+        }
+
+        </main>
+    )
+}
+export default ArticleList;
